326e72a98580d515a0a78cd948e337a60d8ea917
KiamMota <somentekiam@gmail.com>
2025-06-11 17:14:20 -0300

    add : automapper

---
+++ CrossCutting.ObjMapper/Program.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using Domain.Core.Pedido.Interfaces;
+
+namespace CrossCutting.ObjMapper
+{
+    public class PedidoMapper : Profile
+    {
+        private static object createMapper;
+    }
+}
